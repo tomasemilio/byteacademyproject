7b1be39751e76971c0a1e09e665884442bcc02f1
Tomas Silva <tsilvaebensperger@gmail.com>
2016-10-28 14:32:14 -0400

    second update

--- easymoneyfast/api.py
+++ easymoneyfast/api.py
@@ -53,6 +53,46 @@ def logout():
 	session.pop('logged_in', None)
 	return jsonify({'username':'out'})
 
+@app.route('/balance', methods=['POST'])
+def balance():
+	username = request.form.get('username')
+	user = User.query.filter_by(username=username).first().username 
+	user_balance = User.query.filter_by(username=username).first().balance
+
+	return jsonify({'user': user, 'balance': user_balance})
+
+@app.route('/bid100', methods=['POST'])
+def bid100():
+	username = request.form.get('username')
+
+	game = Game.query.filter_by(total_amount=100)[-1]
+	last_player = game.last_user_id
+
+	if username == last_player:
+		return jsonify({'lastuser': 'forbidden'})
+
+
+	query = User.query.filter_by(username=username).first()
+	query.balance = query.balance - 10
+	db.session.commit()
+	new_balance = User.query.filter_by(username=username).first().balance
+
+	game = Game.query.filter_by(total_amount=100)[-1]
+	game.actual_amount = game.actual_amount + 10
+	game.last_user_id = username
+	db.session.commit()
+
+	if game.actual_amount == 100:
+		query = User.query.filter_by(username=username).first()
+		query.balance = query.balance + 100
+		new_game = Game(creation_date=datetime.now(), total_amount=100, actual_amount=0, last_user_id=None)
+		db.session.add(new_game)
+		new_balance = User.query.filter_by(username=username).first().balance
+		db.session.commit()
+
+		return jsonify({'user':username, 'balance':new_balance, 'status': 'done', 'lastuser': None})
+
+	return jsonify({'user':username, 'balance':new_balance, 'status':'open', 'lastuser': game.last_user_id})
 
 
 
